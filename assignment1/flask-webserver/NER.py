17f3d1c05165f97b6b7926a53abef313ecaa5d6e
Ruth Rosenblum <ruth.rosenblum@posh.tech>
2023-03-05 01:58:04 -0500

    made the flask webserver a bit prettier

--- assignment1/flask-webserver/NER.py
+++ assignment1/flask-webserver/NER.py
@@ -3,20 +3,24 @@ import sys, os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.sep.join(dir_path.split(os.path.sep)[:-1]))
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from process_text import SpacyDocument
 
 app = Flask(__name__)
-
+    
 @app.route("/", methods=['GET','POST'])
 def home_page():
-    return render_template('input_page.html')
+    if request and request.form and 'input' in request.form:
+        text = request.form['input']
+    else:
+        text = 'This is some example text with Yangyang and Ruth and Mr. Spongebob Squarepants and a 4th person in Boston on April 25th.'
+    return render_template('input_page.html', displayed_text = text)
 
 @app.post("/processed_text")
 def input_text():
     text = request.form['input_text']
     doc = SpacyDocument(text)
-    return render_template('output_page.html', displayed_text = doc.get_entities_with_markup())
+    return render_template('output_page.html', displayed_text = doc.get_entities_with_markup(), input = text)
 
 if __name__ == '__main__':
     app.run(debug=True)
